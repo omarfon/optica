97efca5215d15e623fb4cdf434e23297b83af581
omar Fonseca <fonsecaom79@gmail.com>
2023-07-04 23:22:07 -0500

    subiendo ventas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { VentasComponent } from './pages/ventas/ventas.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent, VentasComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+            AppRoutingModule,FormsModule, 
+            HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/pages/ventas/ventas.component.ts
+++ src/app/pages/ventas/ventas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PreciosService } from 'src/app/services/precios.service';
+
 
 @Component({
   selector: 'app-ventas',
@@ -18,16 +20,64 @@ export class VentasComponent  implements OnInit {
     direccion:"Los rubies 388 dpto 6",
     fechaNacimiento:"12-01-1979"
   }
-  constructor() { }
+  public estructura: any;
+  public diseno = [];
+  public tecnologys: any = [];
+  public tratamientos: any = [];
+  public producto:any;
+  constructor(public presciosSrv: PreciosService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.presciosSrv.getDataTotal().subscribe((data:any) => {
+      this.estructura = data;
+      console.log('estructura:',this.estructura);
+    })
+
+
+  }
 
   getData(){
     this.data = this.datosUsuario
   }
 
-  saveData(){
-    this.data.push(this.celular,this.email);
+  saveNewUser(){
+    this.data.celular = this.celular;
+    this.data.email = this.email;
     console.log(this.data);
+    localStorage.setItem('user',JSON.stringify(this.data));
+    console.log((localStorage.getItem('user')));
   }
+
+  obtenerTecnologia(event:any){
+    const valor = event.target.value;
+    console.log(valor)
+    const listNew = this.estructura.filter((x:any) => x.DISENO === valor);
+    console.log('DISENO',listNew);
+    const tecnology = listNew.map((e:any) => e.TECNOLOGIA)
+    
+    this.tecnologys = Array.from(new Set(tecnology));
+    console.log('TECNOLOGIAS',this.tecnologys );
+
+  }
+
+  obtenerTratamientos(event:any){
+    const tecno = event.target.value;
+    console.log(tecno, event, this.estructura);
+
+    this.tratamientos = this.estructura.filter((x:any) => x.TECNOLOGIA === tecno);
+    console.log('Lista de tratamientos:',tecno, this.tratamientos);
+
+    this.tratamientos = this.tratamientos.map((e:any) => e.TRATAMIENTOS)
+    
+    this.tratamientos = Array.from(new Set(this.tratamientos));
+    console.log('TRATAMIENTOS',this.tratamientos );
+  }
+
+  obtenerProductos(event:any){
+    const prod = event.target.value;
+    console.log(prod);
+    this.producto = this.estructura.filter((x:any) => x.TRATAMIENTOS === prod);
+    console.log(prod, this.producto);
+  }
+
 }
